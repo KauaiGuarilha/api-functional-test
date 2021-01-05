410569a421c9155e9d41d229ad0f9dfc8e25c6c2
Kauai Guarilha <kauai.guarilha@hotmail.com>
2021-01-04 21:32:09 -0300

    Inclusão da classe de execução

--- src/main/java/br/com/kauai/apifunctionaltest/tasks/functional/Tasks.java
+++ src/main/java/br/com/kauai/apifunctionaltest/tasks/functional/Tasks.java
@@ -1,4 +1,13 @@
 package br.com.kauai.apifunctionaltest.tasks.functional;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Tasks {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tasks.class, args);
+    }
+
 }
